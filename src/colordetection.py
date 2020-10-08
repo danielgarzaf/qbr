51fa9c9001ef12c0d1fff8158a6641cdcbd83c4f
Daniel Garza Flores <danielgarzaf97@gmail.com>
2020-10-07 21:34:41 -0500

    changed values wrapping to allow for weighing each one

--- src/colordetection.py
+++ src/colordetection.py
@@ -23,7 +23,6 @@ class ColorDetection:
         :returns: string
         """
         (h,s,v) = hsv
-
         if definer_flag or custom_flag:
             with open("colors.json", "r") as json_file:
                 custom_colors = json.load(json_file)
@@ -31,7 +30,7 @@ class ColorDetection:
             euclidean_dst = {}
             point_a = (h,s,v)
             for key, value in custom_colors.items():
-                point_b = (tuple(value))
+                point_b = (value[0], value[1], value[2])
                 dst = distance.euclidean(point_a, point_b)
                 euclidean_dst[key] = dst
             color = min(euclidean_dst, key=euclidean_dst.get).lower()
