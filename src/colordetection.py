b7a685aad2a1f1725f62fa0fbe53b65e99f719ce
Daniel Garza Flores <danielgarzaf97@gmail.com>
2020-10-09 02:29:16 -0500

    reversed colordetection.py to original version
    The goal of this decision is to implement a different method for color
    detection in the future involving custom hsv ranges for each color done
    through a better calibration process.

--- src/colordetection.py
+++ src/colordetection.py
@@ -17,39 +17,26 @@ except ImportError as err:
 
 class ColorDetection:
 
-    def get_color_name(self, hsv, definer_flag, custom_flag):
+    def get_color_name(self, hsv):
         """ Get the name of the color based on the hue.
 
         :returns: string
         """
         (h,s,v) = hsv
-        if definer_flag or custom_flag:
-            with open("colors.json", "r") as json_file:
-                custom_colors = json.load(json_file)
-                json_file.close()
-            euclidean_dst = {}
-            point_a = (h,s,v)
-            for key, value in custom_colors.items():
-                point_b = (value[0], value[1], value[2])
-                dst = distance.euclidean(point_a, point_b)
-                euclidean_dst[key] = dst
-            color = min(euclidean_dst, key=euclidean_dst.get).lower()
-            return color
 
-        else:
-            if h < 15 and v < 100:
-                return 'red'
-            elif h <= 10 and v > 100:
-                return 'orange'
-            elif h <= 30 and s <= 100:
-                return 'white'
-            elif h <= 40:
-                return 'yellow'
-            elif h <= 85:
-                return 'green'
-            elif h <= 130:
-                return 'blue'
+        if h < 15 and v < 100:
+            return 'red'
+        elif h <= 10 and v > 100:
+            return 'orange'
+        elif h <= 30 and s <= 100:
             return 'white'
+        elif h <= 40:
+            return 'yellow'
+        elif h <= 85:
+            return 'green'
+        elif h <= 130:
+            return 'blue'
+        return 'white'
 
     def name_to_rgb(self, name):
         """
